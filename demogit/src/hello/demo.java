e35044ea0e99bb5e37254b0cbbc76fd797edc6c5
ramyaasri <raamyaa.sri@gmail.com>
2021-05-26 21:17:00 +0530

    added

---
+++ demogit/src/hello/demo.java
@@ -0,0 +1,8 @@
+package hello;
+
+public class demo {
+	public static void main(String[] args) {
+		System.out.println("this is Demo");
+	}
+
+}
---
+++ demogit/src/welcome/git.java
@@ -0,0 +1,8 @@
+package welcome;
+
+public class git {
+	public static void main(String[] args) {
+		System.out.println("Welcome to git project");
+	}
+
+}
